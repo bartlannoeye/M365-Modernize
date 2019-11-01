0e789f6cb3267db8a94c0eb100d2580f3fd9869a
Bart Lannoeye <bart.lannoeye@gmail.com>
2019-11-01 22:30:07 +0100

    Added XAML Island and InkCanvas

---
+++ src/ContosoExpenses/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoExpenses
+{
+    public static class Program
+    {
+        [STAThread]
+        public static void Main()
+        {
+            using (var xamlApp = new Microsoft.Toolkit.Win32.UI.XamlHost.XamlApplication())
+            {
+                var appOwnedWindowsXamlManager = xamlApp.WindowsXamlManager;
+
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+        }
+    }
+}
